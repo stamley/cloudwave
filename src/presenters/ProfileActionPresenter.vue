0615e72ddd6dababd96545d37a0489455a1a9c98
stamley <axellystam@gmail.com>
2023-04-18 14:18:07 +0200

    Added div with scrolling possibilities, aswell as some CSS

--- src/presenters/ProfileActionPresenter.vue
+++ src/presenters/ProfileActionPresenter.vue
@@ -1,6 +1,7 @@
 <template>
     <LoginView/>
     <CreateAccountView/>
+    <SavedSoundsBoxView></SavedSoundsBoxView>
 </template>
 <script>
 /* 
@@ -11,9 +12,10 @@
 
 import CreateAccountView from '@/views/CreateAccountView.vue';
 import LoginView from '@/views/LoginView.vue';
+import SavedSoundsBoxView from '@/views/SavedSoundsBoxView.vue';
 
 export default{
     name: "ProfileActionPresenter",
-    components: { CreateAccountView, LoginView },
+    components: { CreateAccountView, LoginView, SavedSoundsBoxView },
 }
 </script>
