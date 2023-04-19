4bf725a972237ac1520fa3ab890d7db0d0960dbf
ismailwedihaji <ismailwedihaji@gmail.com>
2023-04-19 12:31:42 +0200

    ModifySounds has been updated

--- src/views/ModifySoundsView.vue
+++ src/views/ModifySoundsView.vue
@@ -1,4 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <label for="music-select">Select a Music:</label>
+    <select v-model="selectedMusic" @change="selectedType">
+      <option
+        v-for="(music, index) in musicOptions"
+        :key="index"
+        :value="music"
+      >
+        {{ music }}
+      </option>
+    </select>
+  </div>
 </template>
-<script></script>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedMusic: "",
+      musicOptions: ["Pop", "Rock", "Jazz", "Classical"],
+    };
+  },
+  methods: {
+    selectedType() {
+      console.log(" you chose " + this.selectedMusic);
+    },
+  },
+};
+</script>
