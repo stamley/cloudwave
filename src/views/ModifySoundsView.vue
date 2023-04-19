94f19a5feedd56d71caec7b9642019fe4054cb62
ismailwedihaji <ismailwedihaji@gmail.com>
2023-04-19 14:28:18 +0200

    update name  of lists

--- src/views/ModifySoundsView.vue
+++ src/views/ModifySoundsView.vue
@@ -18,7 +18,7 @@ export default {
   data() {
     return {
       selectedMusic: "",
-      musicOptions: ["Pop", "Rock", "Jazz", "Classical"],
+      musicOptions: ["Piano", "Guitar", "Flute", "Classical"],
     };
   },
   methods: {
